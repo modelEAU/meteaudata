25c4fb4abf16ed5c7cd9b57ab852520e321293fc
Jean-David Therrien <jeandavidt@gmail.com>
2024-04-04 18:04:14 -0400

    imported classes in root init file

---
+++ src/metEAUdata/__init__.py
@@ -0,0 +1,15 @@
+from metEAUdata.processing_steps.interpolate import linear_interpolation
+from metEAUdata.processing_steps.resample import resample
+from metEAUdata.types import (
+    CalibrationInfo,
+    DataProvenance,
+    Dataset,
+    FunctionInfo,
+    IndexMetadata,
+    Parameters,
+    ProcessingConfig,
+    ProcessingStep,
+    ProcessingType,
+    Signal,
+    TimeSeries,
+)
