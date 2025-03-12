b2f5029447fb60ce32557e53269886935e1b8abe
loesverhaeghe <loeverha.verhaeghe@ugent.be>
2025-03-12 13:57:49 +0100

    added select_time_range function

--- src/meteaudata/__init__.py
+++ src/meteaudata/__init__.py
@@ -6,6 +6,8 @@ from meteaudata.processing_steps.univariate.interpolate import (  # noqa: F401
 )
 from meteaudata.processing_steps.univariate.replace import replace_ranges  # noqa: F401
 from meteaudata.processing_steps.univariate.resample import resample  # noqa: F401
+from meteaudata.processing_steps.univariate.select_time_range import select_time_range  # noqa: F401
+
 from meteaudata.processing_steps.univariate.subset import subset  # noqa: F401
 from meteaudata.types import (  # noqa: F401
     DataProvenance,
